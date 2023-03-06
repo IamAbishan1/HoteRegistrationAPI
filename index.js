f0e986c5d2c7e8b12d076f06c0acf395bebf4da5
Abishan Chemjong <abishanchemjong@gmail.com>
2023-03-06 23:53:58 +0545

    feat: added cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var fs = require("fs");
 
 const multer = require("multer");
 const morgan = require("morgan")
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 const http = require("http");
 
@@ -39,29 +39,67 @@ app.use(request);
 app.use(pathMain);
 app.use(auth);
 app.use(morgan('dev'))
-// app.use(route)
-const validEndpoints = router.use("/",function (req,res,next){
-    next();
-},require("./routes"));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-const error404 = router.use("/", function(req, res, next) {
+// app.use(route)
+const validEndpoints = router.use("/", function (req, res, next) {
+    next();
+}, require("./routes"));
+
+const allowedDomains=[
+    "http://localhost:3000", 
+    "192.168.1.65:3000"
+]
+
+// const corsCheck = router.use((req, res, next) => {
+app.use((req, res, next) => {
+    // console.log("test")
+
+    const origin = req.headers.origin;
+    console.log("Here",req.headers.origin)
+    if (allowedDomains.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+      }
+    
+      res.header("Access-Control-Allow-Credentials", "true");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+      );
+    
+      if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.json({});
+      }
+      next();
+})
+
+const error404 = router.use("/", function (req, res, next) {
     return res.json({
         status: "fail",
         message: "Cannot " + req.method + " " + req.protocol + "://" + req.get("host") + req.originalUrl + ". Maybe the resource was not found or request method is invalid."
     });
 });
 
+// app.use(corsCheck)
+// app.use(
+//     cors({
+//         origin:"http://127.0.0.1:3000/",
+//         methods:["GET","POST"]
+//     })
+// )
 app.use(error404);
 app.use("/public", express.static("public"));
 
-if (!fs.existsSync(__dirname,"images")) {
-    fs.mkdirSync(__dirname ,"images");
+if (!fs.existsSync(__dirname, "images")) {
+    fs.mkdirSync(__dirname, "images");
     console.log("Created new folder: " + __dirname + "images");
 }
 // Static Images Serving from configuration file at /configs/constants
 for (const imagePath in imagePaths) {
     const goodPath = imagePaths[imagePath].split("/").join("/");
-    const imageSavePath = path.join(__dirname,goodPath);
+    const imageSavePath = path.join(__dirname, goodPath);
     //creating folders if they dont exist
     if (!fs.existsSync(imageSavePath)) {
         fs.mkdirSync(imageSavePath);
